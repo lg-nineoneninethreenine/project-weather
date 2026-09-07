b53b8f19636656c842313b2298f9a00d9e119793
jarona! <76845820+im-coder-lg@users.noreply.github.com>
2026-09-07 22:09:08 +0530

    feat: segmented button todo: improve function, slowly integrate tempunit function

--- main_rewrite.py
+++ main_rewrite.py
@@ -12,6 +12,8 @@ app.resizable(False, False)
 
 def search():
     print(f"Searchbar: {searchbar.get()}")
+def tempunit_change(value):
+    print(f"Tempunit: {value}")
 
 heading_label = ctk.CTkLabel(app, text = "Weather", font = ("Consolas", 30)).pack(pady = 5)
 
@@ -24,5 +26,9 @@ searchbar.place(relx=0.33, rely=0.5, anchor=ctk.CENTER)
 searchbutton = ctk.CTkButton(searchframe, text="Search", command=search, width = 40)
 searchbutton.place(relx=0.84, rely=0.5, anchor=ctk.CENTER)
 app.bind("<Return>", lambda event: searchbutton.invoke())
+
+tempbutton = ctk.CTkSegmentedButton(app, values=["C", "F"], command = tempunit_change)
+tempbutton.place(relx=0.85, rely=0.15, anchor=ctk.CENTER)
+tempbutton.set("C")
 
 app.mainloop()
